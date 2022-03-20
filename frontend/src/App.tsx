6c56511ffd109549888b3bf7025b24bfe97098f3
helarious99 <helenahe@ufl.edu>
2022-03-20 16:10:44 -0400

    testing buttons

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -17,6 +17,23 @@ function TickerBtn(props: { onClick: () => void, value: number}) {
 
 }
 
+function bullyGrace() {
+  alert('I <3 DILFS');
+}
+
+/*
+const ButtonGroup = () => {
+  return (
+    <>
+      <button>BOB DUNCAN</button>
+      <button>MR. MOSEBY</button>
+      <button>SHANG CHI'S DAD</button>
+      <button>DICKRELL</button>
+    </>
+  );
+};
+*/
+
 function App() {
 
   const [value, setValue] = useState<number>(0)
@@ -32,8 +49,12 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <SearchBar></SearchBar>
+        <SearchBar/>
         <TickerBtn onClick={() => btnClick()} value={value}/>
+        <button onClick={bullyGrace}>Bully Grace</button>
+        <a href="https://www.youtube.com/c/helarious" target="_blank">
+          <button> Subscribe! </button>
+        </a>
         <a
           className="App-link"
           href="https://reactjs.org"
